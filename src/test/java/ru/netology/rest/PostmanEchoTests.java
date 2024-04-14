a3bb7f164e410330ead0ad15a80fdf8b198a0b82
Yulia Lener <yulias.docs@gmail.com>
2024-04-14 14:13:00 +0700

    Update test

--- src/test/java/ru/netology/rest/PostmanEchoTests.java
+++ src/test/java/ru/netology/rest/PostmanEchoTests.java
@@ -6,17 +6,21 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTests {
+
     @Test
-    void shouldTestSomeData() {
+    void shouldReturnDemoAccounts() {
+// Given - When - Then
+// Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("Good Day")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
                 .when()
                 .post("/post")
+// Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Bad day"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
